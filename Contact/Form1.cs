4f9abbea6bab02a317ff935a697e5f624809f4b7
sarahbichler <sbichler@outlook.com.br>
2024-11-27 14:56:05 -0300

    Adicionar arquivos de projeto.

---
+++ Contact/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact List 1.0.\nWritten by: Sarah Bichler", "About");
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDB1DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'contactDB1DataSet.People'. Você pode movê-la ou removê-la conforme necessário.
+            this.peopleTableAdapter.Fill(this.contactDB1DataSet.People);
+
+        }
+    }
+}
